f555ec620cb2267446b3e3a777759b0e9a6d8d83
Parker Lockhart <parkerlockhart42@gmail.com>
2021-09-01 21:28:48 -0500

    Add animal hash model

--- hash-examples.rb
+++ hash-examples.rb
@@ -5,3 +5,15 @@ car = {
   miles: "300,000",
   condition: "fair"
   }
+
+  animal = {
+    common_name: "red fox",
+    domain: "Eukarya",
+    kingdom: "Animalia",
+    phylum: "Chordata",
+    class: "Mammalia",
+    order: "Carnivora",
+    family: "Canidae",
+    genus: "Vulpes",
+    species: "Vulpes vulpes"
+  }
